c138117bab144f11957effca2015124f4f0e898d
SennDev <GersonEM8900@outlook.com>
2026-01-25 12:23:28 -0600

    Proyecto inicial Angular 20
    Proyecto inicial de angular para la materia de sitios web en primavera del 2026

---
+++ src/app/screens/login-screen/login-screen.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SHARED_IMPORTS } from '../../shared/shared.imports';
+
+@Component({
+  selector: 'app-login-screen',
+  standalone: true,
+  imports: [
+    ...SHARED_IMPORTS
+  ],
+  templateUrl: './login-screen.html',
+  styleUrl: './login-screen.scss',
+})
+export class LoginScreen {
+
+}
